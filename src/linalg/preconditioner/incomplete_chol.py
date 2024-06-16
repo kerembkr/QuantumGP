ec1049b2faccaa42008e7f06c624873f49ba6938
Kerem Bükrü <kerembuekrue@gmail.com>
2024-06-16 09:49:58 +0200

    created new files

--- src/linalg/preconditioner/incomplete_chol.py
+++ src/linalg/preconditioner/incomplete_chol.py
@@ -1,5 +1,5 @@
 import numpy as np
-import random
+
 
 def ichol(a):
     """
@@ -13,12 +13,12 @@ def ichol(a):
     for k in range(n):
         a[k, k] = np.sqrt(a[k, k])
         for i in range(k + 1, n):
-            if (a[i, k] != 0):
+            if a[i, k] != 0:
                 a[i, k] = a[i, k] / a[k, k]
 
         for j in range(k + 1, n):
             for i in range(j, n):
-                if (a[i, j] != 0):
+                if a[i, j] != 0:
                     a[i, j] = a[i, j] - a[i, k] * a[j, k]
 
         for i in range(n):
