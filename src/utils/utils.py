7cede5f63d9be6ba9473876cf61cf02fed1401cd
Kerem Bükrü <kerembuekrue@gmail.com>
2024-06-22 13:25:55 +0200

    merged qutils and utils files

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -56,3 +56,61 @@ def save_fig(name):
     # if not os.path.exists(output_dir):
     #     os.makedirs(output_dir)
     plt.savefig(os.path.join(output_dir, name + ".png"))
+
+
+
+def get_paulis(mat):
+    """
+      Decompose the input matrix into its Pauli components in O(4^n) time
+
+      Args:
+          mat (np.array): Matrix to decompose.
+
+      Returns:
+          mats (list): Pauli matrices
+          wires(list): wire indices, where the Pauli matrices are applied
+
+      """
+
+    # decompose
+    pauli_matrix = qml.pauli_decompose(mat, check_hermitian=True, pauli=False)
+
+    # get coefficients and operators
+    coeffs = pauli_matrix.coeffs
+    ops = pauli_matrix.ops
+
+    # create Pauli word
+    pw = qml.pauli.PauliWord({i: pauli for i, pauli in enumerate(ops)})
+
+    # get wires
+    qubits = [pw[i].wires for i in range(len(pw))]
+
+    # convert Pauli operator to matrix
+    matrices = [qml.pauli.pauli_word_to_matrix(pw[i]) for i in range(len(pw))]
+
+    return matrices, qubits, coeffs
+
+
+def get_random_ls(nqubits, easy_example=False):
+    if easy_example:
+        A_ = np.eye(2 ** nqubits)
+        A_[0, 0] = 2.0
+        b_ = np.ones(2 ** nqubits)
+        return A_, b_
+
+    M = np.random.rand(2 ** nqubits, 2 ** nqubits)
+    A_ = M @ M.T
+    # vector
+    b_ = np.random.rand(2 ** nqubits)
+    b_ = b_ / np.linalg.norm(b_)
+
+    return A_, b_
+
+
+def combine_lists(cost_history):
+    # make one single list
+    cost_history = [item for sublist in cost_history for item in
+                    (sublist if isinstance(sublist, list) else [sublist])]
+
+    return cost_history
+
