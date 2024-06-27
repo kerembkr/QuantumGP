3f89087707325a04320d4e02031389b669856018
Kerem Bükrü <kerembuekrue@gmail.com>
2024-06-27 15:02:02 +0200

    modified pcg class

--- src/linalg/conjugate_gradient.py
+++ src/linalg/conjugate_gradient.py
@@ -22,21 +22,19 @@ def cg(A, b, maxiter=None, tol=1e-8):
     if maxiter is None:
         maxiter = 10 * n
 
+    x = np.zeros(n)  # initial solution guess
+    r = b - A @ x  # initial residual
+    d = r.copy()  # initial search direction
+    i = 0  # iteration counter
+    while np.linalg.norm(r) > tol:  # start CG method
 
+        x = x + (r.T @ r) / (d.T @ (A @ d)) * d  # update solution
+        delta_old = r.T @ r  # save old squared residual
+        r = b - A @ x  # update residual
+        d = r + (r.T @ r / delta_old) * d  # update search direction
 
-    x = np.zeros(n)                                         # initial solution guess
-    r = b - A @ x                                           # initial residual
-    d = r.copy()                                            # initial search direction
-    i = 0                                                   # iteration counter
-    while np.linalg.norm(r) > tol:                          # start CG method
-
-        x = x + (r.T @ r) / (d.T @ (A @ d)) * d             # update solution
-        delta_old = r.T @ r                                 # save old squared residual
-        r = b - A @ x                                       # update residual
-        d = r + (r.T @ r / delta_old) * d                   # update search direction
-
-        i += 1                                              # update iteration counter
-        if i == maxiter:                                    # convergence criteria
+        i += 1  # update iteration counter
+        if i == maxiter:  # convergence criteria
             # raise RuntimeError("No convergence.")           # no convergence
             return x
 
@@ -64,21 +62,21 @@ def cg_winv(A, b, maxiter=None, rtol=1e-6, atol=1e-6):
     if maxiter is None:
         maxiter = 10 * n
 
-    x = np.zeros(n)                                 # initial solution guess
-    C = np.zeros_like(A)                            # inverse approximation
-    r = b - A @ x                                   # initial residual
-    i = 0                                           # iteration counter
-    tol = max(rtol*np.linalg.norm(b), atol)         # threshold
-    while np.linalg.norm(r) > tol:                  # CG loop
-        r = b - A @ x                               # residual
-        s = r                                       # action
-        alpha = s.T @ r                             # observation
-        d = (np.eye(n) - C @ A) @ s                 # search direction
-        eta = s.T @ (A @ d)                         # normalization constant
-        C += 1.0/eta * np.outer(d, d)               # inverse estimate
-        x += alpha / eta * d                        # solution estimate
-        i += 1                                      # update iteration counter
-        if i == maxiter:                            # convergence criteria
+    x = np.zeros(n)  # initial solution guess
+    C = np.zeros_like(A)  # inverse approximation
+    r = b - A @ x  # initial residual
+    i = 0  # iteration counter
+    tol = max(rtol * np.linalg.norm(b), atol)  # threshold
+    while np.linalg.norm(r) > tol:  # CG loop
+        r = b - A @ x  # residual
+        s = r  # action
+        alpha = s.T @ r  # observation
+        d = (np.eye(n) - C @ A) @ s  # search direction
+        eta = s.T @ (A @ d)  # normalization constant
+        C += 1.0 / eta * np.outer(d, d)  # inverse estimate
+        x += alpha / eta * d  # solution estimate
+        i += 1  # update iteration counter
+        if i == maxiter:  # convergence criteria
             # raise RuntimeError("No convergence.")   # no convergence
             return x, C
 
@@ -86,7 +84,6 @@ def cg_winv(A, b, maxiter=None, rtol=1e-6, atol=1e-6):
 
 
 if __name__ == "__main__":
-
     np.random.seed(42)
     N = 3
     A_ = np.random.rand(N, N)
@@ -96,4 +93,4 @@ if __name__ == "__main__":
     xsol1 = cg(A_, b_)
     xsol2, invA = cg_winv(A_, b_)
 
-    print(np.linalg.norm(xsol1-np.linalg.solve(A_, b_)))
+    print(np.linalg.norm(xsol1 - np.linalg.solve(A_, b_)))
--- src/solver/classic/pcg.py
+++ src/solver/classic/pcg.py
@@ -1,55 +1,45 @@
 import numpy as np
+from src.utils.utils import spd
 from src.solver.solver import Solver
-from src.utils.utils import timing
-from src.utils.assertions import (assert_not_none, assert_symmetric, assert_square, assert_not_singular,
-                                  assert_positive_definite)
+from src.linalg.conjugate_gradient import cg, cg_winv
+
 
 class PCG(Solver):
 
-    def __init__(self, M=None, maxiter=None, tol=1e-8):
+    def __init__(self, rank=None, tol=1e-8):
         super().__init__()
-        self.iters = None
-        self.M = M
-        self.maxiter = maxiter
+        self.iters: int = 0
+        self.rank = rank
         self.tol = tol
+        self.invM = None
 
-    @timing
     def solve(self):
         """
-        Conjugate Gradient Method with Preconditioning
+        Solve the linear system of equations Ax=b using the conjugate gradient method
         """
 
-        assert_not_none(self.A, "Matrix A must be set before solving.")
-        assert_not_none(self.b, "Vector b must be set before solving.")
-        # assert_symmetric(self.A)
-        assert_square(self.A)
-        assert_not_singular(self.A)
-        # assert_positive_definite(self.A)
+        self.x, self.invM = cg_winv(A=self.A,
+                                    b=self.b,
+                                    maxiter=self.rank,
+                                    atol=self.tol,
+                                    rtol=self.tol)
 
-        if self.maxiter is None:
-            self.maxiter = 10 * self.N
+        return self.x
 
-        if self.M is None:
-            self.M = np.eye(self.N)
 
-        self.x = np.zeros(self.N)                                       # initial solution guess
-        r = self.b - self.A @ self.x                                    # initial residual
-        z = np.linalg.solve(self.M, r)                                  # apply preconditioner
-        d = z.copy()                                                    # initial search direction
-        delta_new: float = r.T @ z                                      # initial squared residual
-        i: int = 0                                                      # iteration counter
-        while (np.sqrt(delta_new) > self.tol) and (i < self.maxiter):   # start CG method
-            q = self.A @ d                                              # matrix-vector product Ad
-            alpha: float = delta_new / (d.T @ q)                        # step size
-            self.x = self.x + alpha * d                                 # update solution
-            r = r - alpha * q                                           # update residual
-            z = np.linalg.solve(self.M, r)                              # apply preconditioner
-            delta_old: float = delta_new                                # save old squared residual
-            delta_new = r.T @ z                                         # new squared residual
-            beta: float = delta_new / delta_old                         # calculate beta
-            d = z + beta * d                                            # update search direction
-            i += 1                                                      # update iteration counter
-            if i == self.maxiter:                                       # convergence criteria
-                self.iters = self.maxiter                               # maximum number of iterations needed
-                raise BaseException("no convergence")                   # no convergence
-        self.iters = i                                                  # save number of iterations needed
+if __name__ == "__main__":
+
+    # fix random seed
+    np.random.seed(42)
+
+    # linear system
+    N = 200
+    K = spd(N)
+    y = np.random.rand(N)
+
+    # solve system
+    solver = PCG()
+    solver.set_lse(A=K, b=y)
+    x, invK = solver.solve()
+
+    print(np.linalg.norm(x - np.linalg.solve(K, y)))
