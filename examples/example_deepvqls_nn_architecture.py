b7461c87407c04be48333fe546693b0e80da711d
Kerem Bükrü <kerembuekrue@gmail.com>
2024-07-07 12:48:50 +0200

    modified deepvqls nn architecture example

--- examples/example_deepvqls_nn_architecture.py
+++ examples/example_deepvqls_nn_architecture.py
@@ -16,7 +16,7 @@ nlayers = 1
 
 maxiter = 100
 
-nsolvers = 10
+nsolvers = 3
 
 # choose ansatz, state preparation, backend
 ansatz_ = HardwareEfficient(nqubits=nqubits, nlayers=nlayers)
@@ -46,6 +46,29 @@ for i in range(nsolvers):
 
     # loss curves
     cost_hists["ninputs_{:d}".format(ninputs)] = solver.loss
+
+
+for i in range(nsolvers):
+
+    ninputs = 4
+    nhidden = 8*(i+1)
+
+    # init
+    solver = DeepVQLS()
+
+    # set linear system
+    A0, b0 = get_random_ls(nqubits, easy_example=True)  # random spd
+    solver.set_lse(A=A0, b=b0)
+
+    # setup
+    solver.setup(optimizer=SGDTorch(), ansatz=ansatz_, stateprep=prep_, backend=backend_, epochs=maxiter, tol=1e-5,
+                 ninputs=ninputs, nhidden=nhidden)
+
+    # solve linear system
+    solver.solve()
+
+    # loss curves
+    cost_hists["nhidden_{:d}".format(nhidden)] = solver.loss
 
 plot_costs(data=cost_hists, save_png=True, title=None, fname="deep_vqls_nn_architecture_hea_nq{:d}_nl{:d}".format(
     nqubits, nlayers))
