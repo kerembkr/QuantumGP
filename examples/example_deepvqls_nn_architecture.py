03b2c1fa0fa09decad442f1c7cf0b9885540dfc0
Kerem Bükrü <kerembuekrue@gmail.com>
2024-07-07 15:27:18 +0200

    modified example_deepvqls_nn_architecture.py

--- examples/example_deepvqls_nn_architecture.py
+++ examples/example_deepvqls_nn_architecture.py
@@ -1,6 +1,6 @@
 from src.utils.ansatz import HardwareEfficient
 from src.utils.embedding import MottonenStatePrep
-from src.optimizers.optim_torch import AdamTorch, AdagradTorch, RMSPropTorch, SGDTorch
+from src.optimizers.optim_torch import SGDTorch
 from src.utils.backend import DefaultQubitTorch
 from src.utils.utils import get_random_ls
 from src.solver.quantum.vqls.vqls_dnn import DeepVQLS
@@ -14,9 +14,9 @@ np.random.seed(42)
 nqubits = 1
 nlayers = 1
 
-maxiter = 100
+maxiter = 1000
 
-nsolvers = 3
+nsolvers = 4
 
 # choose ansatz, state preparation, backend
 ansatz_ = HardwareEfficient(nqubits=nqubits, nlayers=nlayers)
@@ -27,48 +27,28 @@ cost_hists = {}
 
 for i in range(nsolvers):
 
-    ninputs = 4*(i+1)
-    nhidden = 8
+    ninputs = 2*(i+1)  # number of inputs for the neural network
 
-    # init
-    solver = DeepVQLS()
+    for j in range(nsolvers):
 
-    # set linear system
-    A0, b0 = get_random_ls(nqubits, easy_example=True)  # random spd
-    solver.set_lse(A=A0, b=b0)
+        nhidden = 2*(j+1)  # number of nodes per hidden layer
 
-    # setup
-    solver.setup(optimizer=SGDTorch(), ansatz=ansatz_, stateprep=prep_, backend=backend_, epochs=maxiter, tol=1e-5,
-                 ninputs=ninputs, nhidden=nhidden)
+        # init
+        solver = DeepVQLS()
 
-    # solve linear system
-    solver.solve()
+        # set linear system
+        A0, b0 = get_random_ls(nqubits, easy_example=True)  # random spd
+        solver.set_lse(A=A0, b=b0)
 
-    # loss curves
-    cost_hists["ninputs_{:d}".format(ninputs)] = solver.loss
+        # setup
+        solver.setup(optimizer=SGDTorch(eta=0.01), ansatz=ansatz_, stateprep=prep_, backend=backend_, epochs=maxiter, tol=1e-5,
+                     ninputs=ninputs, nhidden=nhidden)
 
+        # solve linear system
+        solver.solve()
 
-for i in range(nsolvers):
-
-    ninputs = 4
-    nhidden = 8*(i+1)
-
-    # init
-    solver = DeepVQLS()
-
-    # set linear system
-    A0, b0 = get_random_ls(nqubits, easy_example=True)  # random spd
-    solver.set_lse(A=A0, b=b0)
-
-    # setup
-    solver.setup(optimizer=SGDTorch(), ansatz=ansatz_, stateprep=prep_, backend=backend_, epochs=maxiter, tol=1e-5,
-                 ninputs=ninputs, nhidden=nhidden)
-
-    # solve linear system
-    solver.solve()
-
-    # loss curves
-    cost_hists["nhidden_{:d}".format(nhidden)] = solver.loss
+        # loss curves
+        cost_hists["ni_{:d} nh_{:d}".format(ninputs, nhidden)] = solver.loss
 
 plot_costs(data=cost_hists, save_png=True, title=None, fname="deep_vqls_nn_architecture_hea_nq{:d}_nl{:d}".format(
     nqubits, nlayers))
